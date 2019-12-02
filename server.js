20bdca4c9c752034b15124a8424b718e7aa647be
Ameyg1 <ameyg1@gmail.com>
2019-12-02 17:52:53 +1300

    prod env

--- server.js
+++ server.js
@@ -74,6 +74,15 @@ app.post('/api/attendees',(req, res) => {
 //});
  
 //Server listening
+if(process.env.NODE_ENV=== 'production')
+  {
+    //setstatic folder
+    app.use(express.static('client/build'));
+    app.get('*',(req,res)=>{
+      res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+
+    });
+  }
 app.timeout = 0;
 const port= process.env.PORT || 5000;
 app.listen(port,() =>{
